20d6752a714fad66ef6abbd8bf37bbefa7662130
thien0411 <thien.nguyen0411@gmail.com>
2017-03-29 20:58:13 -0600

    Add checking credit info

--- src/controller/Bank.java
+++ src/controller/Bank.java
@@ -1,10 +1,17 @@
 package controller;
 
-import java.io.IOException;
+import java.io.IOException; 
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.BankInfo;
 
 /**
  * Servlet implementation class Bank
@@ -25,6 +32,28 @@ public class Bank extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String cardType = request.getParameter("cardType");
+		String cardNumber = request.getParameter("cardNumber");
+		String securityCode = request.getParameter("securityCode");
+		String expireMonth = request.getParameter("expireMonth");
+		Double totalOrder = Double.parseDouble(request.getParameter("totalOrder"));
+		
+		
+		int transactionStatus = 0;
+		
+		BankInfo aCreditCard = new BankInfo();
+		
+		aCreditCard.setCardNumber(cardNumber);
+		aCreditCard.setCardType(cardType);
+		aCreditCard.setSecurityCode(securityCode);
+		aCreditCard.setExpireMonth(expireMonth);
+		
+		if(aCreditCard.validData() && aCreditCard.sufficientFunds(totalOrder))
+			transactionStatus = 1;
+
+		PrintWriter out = response.getWriter(); 
+		out.println(transactionStatus);
+	
 	}
 
 	/**
---
+++ src/model/BankInfo.java
@@ -0,0 +1,72 @@
+package model;
+
+public class BankInfo {
+	private String cardType;
+	private String cardNumber;
+	private String securityCode;
+	private String expireMonth;
+	private double balance;
+	
+
+	public BankInfo() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public String getCardType() {
+		return cardType;
+	}
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getSecurityCode() {
+		return securityCode;
+	}
+	public void setSecurityCode(String securityCode) {
+		this.securityCode = securityCode;
+	}
+	public String getExpireMonth() {
+		return expireMonth;
+	}
+	public void setExpireMonth(String expireMonth) {
+		this.expireMonth = expireMonth;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public boolean validData () {
+		boolean isValid = true;
+
+		if (this.cardNumber == null || this.cardNumber.trim().equals("") || this.cardNumber.length() != 16 )
+			isValid = false;
+
+		if (this.securityCode == null || this.securityCode.trim().equals("")) {
+			if (!(this.securityCode.trim().length() == 3 || this.securityCode.trim().length() == 4))
+				isValid = false;
+		}
+		if (this.cardType == null || this.cardType.trim().equals("")) isValid = false;
+		if (this.expireMonth == null || this.expireMonth.trim().equals("")) isValid = false;
+
+		return isValid;
+	
+	}
+	
+	public boolean sufficientFunds (double totalOrder) {
+		if(totalOrder > balance) return false;
+		else {
+				balance -= balance -  totalOrder;
+				return true;
+		}
+	}
+
+}
