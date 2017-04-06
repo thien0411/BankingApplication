699cb092806da3c5adf74b2084d282f3a3c9946f
Mark Hernandez <mh81796@gmail.com>
2017-04-06 15:41:05 -0500

    Finish phase 3

--- src/controller/Bank.java
+++ src/controller/Bank.java
@@ -2,15 +2,10 @@ package controller;
 
 import java.io.IOException; 
 import java.io.PrintWriter;
-import java.util.LinkedList;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import model.BankInfo;
 
 /**
@@ -24,20 +19,17 @@ public class Bank extends HttpServlet {
      */
     public Bank() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		String cardType = request.getParameter("cardType");
 		String cardNumber = request.getParameter("cardNumber");
 		String securityCode = request.getParameter("securityCode");
 		String expireMonth = request.getParameter("expireMonth");
 		Double totalOrder = Double.parseDouble(request.getParameter("totalOrder"));
-		
 		
 		int transactionStatus = 0;
 		
@@ -48,19 +40,19 @@ public class Bank extends HttpServlet {
 		aCreditCard.setSecurityCode(securityCode);
 		aCreditCard.setExpireMonth(expireMonth);
 		
-		if(aCreditCard.validData() && aCreditCard.sufficientFunds(totalOrder))
+		if(aCreditCard.validData() && aCreditCard.sufficientFunds(totalOrder)) {
 			transactionStatus = 1;
+		}
 
 		PrintWriter out = response.getWriter(); 
 		out.println(transactionStatus);
-	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
